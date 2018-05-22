77e97ee4b6a7f6661a422eac8b04a0cfaeb23b7c
Nuno Leite de Faria <nleitefaria@gmail.com>
2018-05-22 14:15:42 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Home from './Home.js';
+import About from './About.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>        
+      	<Link to={`/home`}>Home</Link>
+      	<Link to={`/about`}>About</Link>         
+      	<Switch>        
+          	<Route path='/home' component={Home}/>
+          	<Route path='/about' component={About}/>
+        </Switch>
       </div>
     );
   }
