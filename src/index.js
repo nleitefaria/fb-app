28b70b2a797132086edf480de3493b436dbc2548
Nuno Leite de Faria <nleitefaria@gmail.com>
2018-05-24 14:28:50 +0100

    Added react-grid

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
